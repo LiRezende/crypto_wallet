9e4c3ded0d63eb32b7b4be5cfe9057e25e741e10
LiRezende <librusamolin@gmail.com>
2022-08-03 14:45:44 -0300

    Database seeding with Coins records.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+coins = [
+        {
+            description: "Bitcoin",
+            acronym: "BTC",
+            url_image: "https://imagensemoldes.com.br/wp-content/uploads/2020/09/Logo-Bitcoin-PNG.png"
+        },
+        {
+            description: "Ethereum",
+            acronym: "ETH",
+            url_image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Ethereum_Classic_Logo.svg/633px-Ethereum_Classic_Logo.svg.png"
+        },
+        {
+            description: "Dashcoin",
+            acronym: "DSH",
+            url_image: "https://cdn4.iconfinder.com/data/icons/crypto-currency-and-coin-2/256/dash_dashcoin-512.png"
+        }
+]
+
+coins.each |coin| do 
+    Coin.find_or_create_by!(coin)
+end
